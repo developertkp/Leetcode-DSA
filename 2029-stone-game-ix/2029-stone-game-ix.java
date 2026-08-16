23707a4878a847cdc5e5462dde5390a0002b3c13
Tusharkanti Pal <tusharkantipal2003@gmail.com>
2026-08-16 22:30:22 +0530

    Time: 3 ms (100%), Space: 114.9 MB (52.03%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+        int[] arr=new int[3];
+        for(int stone : stones){
+            arr[stone%3]++;
+        }
+        if(arr[0]%2 == 0){
+            return arr[1]>0 && arr[2]>0;
+        }
+        return Math.abs(arr[1]-arr[2])>2;
+    }
+}
